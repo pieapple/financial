51aa583a8caf7f33de3191ef8a2f5bb0cff18b1f
Zhihong Liu <zhiholiu@cisco.com>
2018-09-27 14:41:50 +0800

    use urllib

--- finacial.py
+++ finacial.py
@@ -1,10 +1,10 @@
 # -*- coding:utf-8 -*-
-import urllib2
+import urllib
 
 # url="http://cffex.com.cn/ccpm/?productid=T"
-# response = urllib2.urlopen(url)
-# print response.read()
+# res = urllib.urlopen(url)
+# print res.read()
 
 url="http://cffex.com.cn/sj/ccpm/201507/01/T.xml"
-response = urllib2.urlopen(url)
-print response.read()
+res = urllib.urlopen(url)
+print res.read()
