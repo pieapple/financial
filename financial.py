a270aad3946b4b87126cb46dddb584cbad6b0a75
Zhihong Liu <zhiholiu@cisco.com>
2018-09-29 08:43:49 +0800

    grab the right data

--- financial.py
+++ financial.py
@@ -8,30 +8,39 @@ import xlwt
 
 def retrieve_data(url):
     res = urllib.urlopen(url)
-    text = res.read()
+    if res.code != 200:
+        return None
+
     try:
+        text = res.read()
         root = ET.fromstring(text)
         return root
     except Exception as e:
         return None
 
 
+def add_header(ws):
+    ws.write(0, 0, "date")
+    ws.write(0, 1, "position")
+    ws.write(0, 2, "volume amount")
+    ws.write(0, 3, "largest buyer")
+    ws.write(0, 4, "largest seller")
+
+
 def add_row(ws, index, date, root):
     ws.write(index, 0, date)
-    ws.write(index, 1, root[-2][2].text)
-    ws.write(index, 2, root[1][4].text)
-    ws.write(index, 3, root[2][4].text)
+    ws.write(index, 2, int(root[-2][2].text))
+    ws.write(index, 3, root[1][4].text)
+    ws.write(index, 4, root[2][4].text)
+
 
 wb = xlwt.Workbook()
 ws = wb.add_sheet("summary")
-ws.write(0, 0, "date")
-ws.write(0, 1, "volume amount")
-ws.write(0, 2, "largest buyer")
-ws.write(0, 3, "largest seller")
+add_header(ws)
 
 row = 0
 start_date = date(2015, 7, 1)
-#end_date = date(2015, 8, 1)
+# end_date = date(2015, 8, 1)
 end_date = date.today()
 for n in range(int ((end_date - start_date).days)):
     single_date = start_date + timedelta(n)
@@ -46,4 +55,4 @@ for n in range(int ((end_date - start_date).days)):
     print "adding row", row, date
     add_row(ws, row, date, root)
 
-wb.save("financial_data.xls")
+wb.save("result.xls")
